66899fdf6412c608863e591e0453d629791a5653
Igor Polyakov <igor.polyakov@elecard.ru>
2018-05-21 12:23:56 +0700

    set default page

--- rails_backend/config/routes.rb
+++ rails_backend/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :search_queries
+  root 'search_queries#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
