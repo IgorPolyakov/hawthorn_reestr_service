629808dc46bd603c8de336984a8bf5a2e67998f7
IgorPolyakov <igorpolyakov@protonmail.com>
2018-05-26 12:58:49 +0700

    small fix for js

--- rails_backend/app/assets/javascripts/search_queries.js
+++ rails_backend/app/assets/javascripts/search_queries.js
@@ -1,20 +1,30 @@
 function status() {
   $('.badge').each(function(i, ele) {
     let val = $(ele).text();
-    if (val === "ошибка") {
-      $(ele).addClass('badge-danger');
+
+    // if value like this - 0/0
+    if (val.match(/\d+\/\d+/ig)) {
+      let result = val.split('/');
+      if (result[0] === result[1]) {
+        $(ele).addClass('badge-success');
+      } else {
+        $(ele).addClass('badge-warning');
+      }
     }
-    else if (val === "готово") {
-      $(ele).addClass('badge-success');
-    }
-    else if (val === "в обработке") {
-      $(ele).addClass('badge-warning');
-    }
-    let result = val.split('/');
-    if (result[0] === result[1]) {
-      $(ele).addClass('badge-success');
-    } else if (val.match(/\d+\/\d+/ig)) {
-      $(ele).addClass('badge-warning');
+    // else color status
+    else {
+      if (val === "ошибка") {
+        $(ele).addClass('badge-danger');
+      }
+      else if (val === "готово") {
+        $(ele).addClass('badge-success');
+      }
+      else if (val === "в обработке") {
+        $(ele).addClass('badge-warning');
+      }
+      else   {
+        $(ele).addClass('badge-danger');
+      }
     }
   })
 }
