bc4728c53a9cb8b4c9905b48fe0d1498a51f7570
neverhooda <neverhooda@gmail.com>
2018-05-30 09:40:10 +0700

    cleanup

--- selenium_py/zip_loader.py
+++ selenium_py/zip_loader.py
@@ -43,7 +43,7 @@ def obj_dict(obj):
 parser = argparse.ArgumentParser(description='Welcome to the help for zip loader v.1.')
 parser.add_argument("-v", "--virtual", dest='virtual', action='store_true', help="Enabled useg virtual display.")
 parser.add_argument("-t", "--token", dest='token', nargs = '?', type = str, default = "c5793610-b33b-476f-bebf-53a0f1366383", help="Set token for loggin on site, it's have default value.")
-parser.add_argument("-q", "--query", dest='query', nargs = '?', type = str, default = '[{"id":1,"id_location":1,"search_uid":"80-39089153"}]', help="As a query, specify the search_uid. The query must be in the jason.")
+parser.add_argument("-q", "--query", dest='query', nargs = '?', type = str, default = '[{"id":1,"location_id":1,"search_uid":"80-39089153"}]', help="As a query, specify the search_uid. The query must be in the jason.")
 # parser.add_argument("-q", "--query", dest='query', nargs = '?', type = str, default = '80-39089153,80-39089149', help="As a query, specify the search_uid. uid separate by ','.")
 parser.add_argument("-f", "--file", dest='onFile', action='store_true', help="Send result to file bin/result.json.")
 parser.add_argument("-http", "--http", dest='onHttp', action='store_true', help="Send result to http url.")
@@ -108,7 +108,7 @@ for search_uid in search_uids:
 	i = 1
 	my_query = models.QueryResult()
 	my_query.id = search_uid.id
-	my_query.id_location = search_uid.id_location
+	my_query.location_id = search_uid.location_id
 	browser.find_element_by_xpath(ResponseMenu.search_field).send_keys(search_uid.search_uid)
 	browser.find_element_by_xpath(ResponseMenu.search_btn).click()
 
@@ -131,28 +131,7 @@ for search_uid in search_uids:
 		print("[WARNING] ZIP file was not found!")
 
 	browser.find_element_by_xpath(ResponseMenu.search_reset).click()
-	time.sleep(2) # for test
-
-
-# if total_row < ResponseMenu.max_row:
-# 	count_row = total_row
-# else:
-# 	count_row = ResponseMenu.max_row
-
-# my_querys = []
-# for i in range(1,count_row+1):
-# 	my_query = models.QueryKdr()
-# 	my_query.search_uid = browser.find_element_by_xpath(ResponseMenu.getTableValue(i,1)).text
-# 	my_query.date_request = browser.find_element_by_xpath(ResponseMenu.getTableValue(i,2)).text
-# 	my_query.status = browser.find_element_by_xpath(ResponseMenu.getTableValue(i,3)).text
-# 	btn_load = browser.find_element_by_xpath(ResponseMenu.getTableValue(i,4))
-# 	my_query.zip_url = btn_load.get_attribute("href")
-# 	if my_query.search_uid in search_uids:
-# 		btn_load.click()
-# 		time.sleep(5) # for test
-# 		my_query.root_path = "%s/Response-%s.zip"%(os.path.expanduser(args.output),my_query.search_uid)
-# 		my_querys.append(my_query)
-# 		#wait for download
+	time.sleep(1) # for test
 
 json_string = json.dumps(my_querys,default=obj_dict,sort_keys=True,indent=4)
 print(json_string)
